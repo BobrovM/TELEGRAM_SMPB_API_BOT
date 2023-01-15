6bad5b39b74c1d1511005c6345d354d1c9f2178e
Mikhail Bobrov <BOBRTVLOL@gmail.com>
2023-01-15 18:47:19 +0100

    Buttons started kinda working.

--- main.py
+++ main.py
@@ -7,17 +7,46 @@ import telegram.ext as te
 
 logger = logging.getLogger(__name__)
 
+VIDEO_BUTTON = "Video"
+IMAGE_BUTTON = "Image"
 
-async def start(update: t.Update, context: te.ContextTypes.DEFAULT_TYPE) -> None:
-    user = update.effective_user
-    await update.message.reply_html(
-        rf"Hi {user.mention_html()}!",
-        reply_markup=t.ForceReply(selective=True)
+FIRST_MENU_MARKUP = t.InlineKeyboardMarkup([
+    [t.InlineKeyboardButton(VIDEO_BUTTON, callback_data=VIDEO_BUTTON)],
+    [t.InlineKeyboardButton(IMAGE_BUTTON, callback_data=IMAGE_BUTTON)]
+])
+
+
+async def start(update: t.Update, context: te.CallbackContext) -> None:
+    await context.bot.send_message(
+        update.message.from_user.id,
+        text="TEXT",
+        parse_mode=t.constants.ParseMode.HTML,
+        reply_markup=FIRST_MENU_MARKUP
+    )
+
+
+async def button_tap(update: t.Update, context: te.CallbackContext) -> None:
+    data = update.callback_query.data
+    text = ""
+    print(data)
+
+    if data == VIDEO_BUTTON:
+        text = VIDEO_BUTTON
+    elif data == IMAGE_BUTTON:
+        text = IMAGE_BUTTON
+
+    await update.callback_query.answer()
+
+    await context.bot.send_message(
+        update.callback_query.from_user.id,
+        text=text,
+        parse_mode=t.constants.ParseMode.HTML,
+        reply_markup=FIRST_MENU_MARKUP
     )
 
 
 async def help_command(update: t.Update, context: te.ContextTypes.DEFAULT_TYPE) -> None:
-    await update.message.reply_text("Help!")
+    await update.message.reply_text("WIP!")
 
 
 async def echo(update: t.Update, context: te.ContextTypes.DEFAULT_TYPE) -> None:
@@ -31,6 +60,8 @@ def main():
     application.add_handler(te.CommandHandler("help", help_command))
 
     application.add_handler(te.MessageHandler(te.filters.TEXT & ~te.filters.COMMAND, echo))
+
+    application.add_handler(te.CallbackQueryHandler(button_tap))
 
     application.run_polling()
 
